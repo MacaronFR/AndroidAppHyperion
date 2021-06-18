bee5c766d633b2a6de82be23db8960c181ab065d
MacaronFR <denisft77@gmail.com>
2021-06-18 15:50:53 +0200

    try and secret zone

---
+++ app/src/main/java/fr/macaron_dev/hyperion/activity/KaamelottActivity.kt
@@ -0,0 +1,14 @@
+package fr.macaron_dev.hyperion.activity
+
+import android.os.Bundle
+import android.view.WindowInsetsController
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import fr.macaron_dev.hyperion.R
+
+class KaamelottActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kaamelott)
+    }
+}
--- app/src/main/java/fr/macaron_dev/hyperion/ui/about/AboutFragment.kt
+++ app/src/main/java/fr/macaron_dev/hyperion/ui/about/AboutFragment.kt
@@ -1,11 +1,17 @@
 package fr.macaron_dev.hyperion.ui.about
 
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import fr.macaron_dev.hyperion.R
+import fr.macaron_dev.hyperion.activity.KaamelottActivity
 
 class AboutFragment : Fragment() {
 
@@ -14,6 +20,19 @@ class AboutFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        val root = inflater.inflate(R.layout.fragment_about, container, false)
+        val logo = root.findViewById<ImageView>(R.id.logo)
+        val copyright = root.findViewById<TextView>(R.id.copyright)
+        copyright.setOnClickListener {
+            val builder = AlertDialog.Builder(root.context)
+            builder.setTitle("Étalon du Cul")
+            builder.setMessage("Étalon du Cul veut te dire bonjour")
+            builder.create().show()
+        }
+        logo.setOnClickListener {
+            val intent = Intent(context, KaamelottActivity::class.java)
+            startActivity(intent)
+        }
+        return root
     }
 }
